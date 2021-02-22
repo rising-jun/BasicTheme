e7b3169e849475c113f4c955a29b9026a6453b06
kimdongjune <jun36817@naver.com>
2021-02-22 17:38:39 +0900

    210222
    complete

---
+++ BasicTheme/thirdpage/NewsPagerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdPagerViewController.swift
+//  BasicTheme
+//
+//  Created by 김동준 on 2021/02/16.
+//
+
+import Foundation
+import UIKit
+
+class ThirdPagerViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+    }
+
+
+}
+
